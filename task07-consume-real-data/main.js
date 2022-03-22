71875f5163cf06fd866fefd2fde3810cad7db22b
pdinarte <99428207+pdinarte@users.noreply.github.com>
2022-03-22 12:43:28 -0600

    Task 07 files

---
+++ task07-consume-real-data/main.js
@@ -0,0 +1,22 @@
+const readMoreBtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.show-text');
+const logo = document.querySelector('.main__hero-logo');
+const menuBtn = document.querySelector('.nav__menu')
+const dropdown = document.querySelector('.nav__dropdown');
+
+readMoreBtn.addEventListener('click', (e)=>{
+  logo.classList.toggle('visible');
+  text.classList.toggle('visible');
+  if(readMoreBtn.innerText === 'Read All'){
+    readMoreBtn.innerHTML = readMoreBtn.innerHTML.replace('Read All', 'Read Less');
+    readMoreBtn.innerHTML = readMoreBtn.innerHTML.replace('fa-chevron-down', 'fa-chevron-up');
+  } else {
+    readMoreBtn.innerHTML = readMoreBtn.innerHTML.replace('Read Less', 'Read All');
+    readMoreBtn.innerHTML = readMoreBtn.innerHTML.replace('fa-chevron-up', 'fa-chevron-down');
+  }
+});
+
+menuBtn.addEventListener('click', (e)=> {
+  dropdown.classList.toggle('visible');
+  menuBtn.classList.toggle('fa-xmark');
+});
