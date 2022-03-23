9011e0131138146b271f9a450a23760da2093db3
pdinarte <99428207+pdinarte@users.noreply.github.com>
2022-03-23 14:48:18 -0600

    Requested changes for data.js

--- task07-consume-real-data/data.js
+++ task07-consume-real-data/data.js
@@ -1,41 +1,40 @@
 //import fetch from 'node-fetch';
 
+const URL = 'https://static.akurey.com/trainings/OnePieceInformation.json';
 var characters = [];
 var islands = [];
 var mysticObjects = [];
 
-const sortByName = function(a, b){
-  let x = a.name.toLowerCase();
-  let y = b.name.toLowerCase();
-  if (x < y) {return -1;}
-  if (x > y) {return 1;}
+const sortByName = function(object1, object2){
+  let name1 = object1.name.toLowerCase();
+  let name2 = object2.name.toLowerCase();
+  if (name1 < name2) {return -1;}
+  if (name1 > name2) {return 1;}
   return 0;
 };
 
 function fetchData() {
-  fetch('https://static.akurey.com/trainings/OnePieceInformation.json')
-  .then( response => response.json())
+  fetch( URL )
+  .then( response => response.json() )
   .then( data => {
     characters = data.characters;
     islands = data.islands;
     mysticObjects = data.mysticObjects;
-  })
-  .then( () => {
     characters.sort(sortByName);
     islands.sort(sortByName);
     mysticObjects.sort(sortByName);
     createCharacters(characters.slice(0,4));
     createIslands(islands.slice(0,3));
-    createobjects(mysticObjects.slice(0,4));
+    createObjects(mysticObjects.slice(0,4));
   })
-  .catch((e) => console.log(e))
+  .catch((e) => console.log('Something went wrong while fetching data from '+URL+'. See error below:\n'+ e))
 };
 
 function createCharacters(array) {
-  let monkey_img = "img--center"
-  let nico_img = "img--nico"
-  let portgas_img = "img--portgas"  
-  var html = array.map(charact => {
+  const monkey_img = "img--center"
+  const nico_img = "img--nico"
+  const portgas_img = "img--portgas"  
+  const html = array.map(charact => {
     return `<div class="character-card">
               <img class="card-grid__img ${charact.img == "monkeydluffy.svg" ? monkey_img : 
                                            charact.img == "nico.svg" ? nico_img : 
@@ -47,20 +46,20 @@ function createCharacters(array) {
 }
 
 function createIslands(array) {
-  var html = array.map(isla => {
+  const html = array.map(island => {
     return `<div class="island-card">
-              <img class="card-grid__img" src="images/${isla.img}">
+              <img class="card-grid__img" src="images/${island.img}">
               <div class="island-card__infobox">
-                <p class="l3">${isla.name}</p>
-                <p class="p-small"><i class="fa-solid fa-location-dot"></i> ${isla.Location}</p>
+                <p class="l3">${island.name}</p>
+                <p class="p-small"><i class="fa-solid fa-location-dot"></i> ${island.Location}</p>
               </div>
             </div>`;
   }).join("");
   document.querySelector("#islands-section").insertAdjacentHTML("afterbegin", html);
 }
 
-function createobjects(array) {
-  var html = array.map(myst => {
+function createObjects(array) {
+  const html = array.map(myst => {
     return `<div class="mist-grid__card">
               <img src="images/${myst.img}">
               <div class="mist-grid__card-infobox">
