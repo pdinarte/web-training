19f8f8d3aa3c3b0be53d9e1751565995530719fd
pdinarte <99428207+pdinarte@users.noreply.github.com>
2022-04-07 11:23:08 -0600

    Fixed images position

--- task10-vue/one-piece-site/src/components/cards/CharacterCard.vue
+++ task10-vue/one-piece-site/src/components/cards/CharacterCard.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="character-card">
-    <img class="card-grid__img" :src=image alt=""/>
+    <img class="card-grid__img" 
+        :class="{'img--center' : image === '/images/monkeydluffy.svg', 
+                 'img--nico' : image === '/images/nico.svg', 
+                 'img--portgas' : image === '/images/ace.svg'}" :src=image alt=""/>
     <h3 class="character-card__name">{{name}}</h3>
   </div>
 </template>
