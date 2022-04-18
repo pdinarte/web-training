b478b8752c1bbbb0d00e488c5eadac3dae9b3e3d
pdinarte <99428207+pdinarte@users.noreply.github.com>
2022-04-18 14:57:00 -0600

    Update SearchBar.vue

--- task10-vue/one-piece-site/src/components/SearchBar.vue
+++ task10-vue/one-piece-site/src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="main__search-bar side-margin">
     <i class="fa-solid fa-magnifying-glass"></i>
-    <p class="p-medium">Search Characters, Islands or Objects</p>
+    <input class="search-bar" type="text" placeholder="Search Characters, Islands or Objects">
   </section>
 </template>
 
@@ -10,3 +10,19 @@ export default {
   name : 'SearchBar'
 }
 </script>
+
+<style >
+  .search-bar {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: 0.25px;
+    border: none;
+    width: 100%;
+  }
+  .search-bar:focus {
+    outline: none;
+  }
+</style>
